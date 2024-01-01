9e1863af489c76e6738d068996adb3cffd73026c
+ <andrewjohnjulian@gmail.com>
2023-12-31 17:12:57 -0700

    added loading wheel

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function fetchData() {
   )
     .then((response) => response.json())
     .then((data) => {
-      allData = data.filter((item) => item.Display === true); // Filter for Display: true
-      displayData(allData); // Display only filtered data
+      allData = data.filter((item) => item.Display === true);
+      displayData(allData);
     })
     .catch((error) => {
       document.getElementById("api-data").innerText = "Error: " + error;
@@ -18,21 +18,18 @@ function fetchData() {
 
 function displayData(data) {
   const container = document.getElementById("api-data");
-  container.innerHTML = ""; // Clear existing content
+  container.innerHTML = "";
 
-  // Check if data is not empty
   if (data.length === 0) {
     container.innerHTML = "<p>No data to display.</p>";
     return;
   }
 
-  // Create and append elements for each item in the data
   data.forEach((item) => {
     console.log(item.Date);
     const itemDiv = document.createElement("div");
-    itemDiv.classList.add("event"); // Optional: for styling
+    itemDiv.classList.add("event");
 
-    // Assuming 'item' has properties you want to display, for example, 'name' and 'value'
     itemDiv.innerHTML = `
           <p>Name: ${item.title}</p>
      
